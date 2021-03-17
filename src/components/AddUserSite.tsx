63b22d15953b722ee756c5269752d34c1b919c73
Bill Klinkow <klinkow@gmail.com>
2021-03-17 11:48:21 -0700

    Add name to user sites

--- src/components/AddUserSite.tsx
+++ src/components/AddUserSite.tsx
@@ -2,24 +2,25 @@ import React from 'react'
 
 type AddUserSiteProps = {
   onClose: () => any
-  onAddUserSite: (lat: number, lon: number) => any
+  onAddUserSite: (lat: number, lon: number, label: string) => any
 }
 
 type AddUserSiteState = {
   lat: string
   lon: string
+  label: string
 }
 
 export default class AddUserSite extends React.Component<AddUserSiteProps, AddUserSiteState> {
   constructor(props: any) {
     super(props)
 
-    this.state = { lat: '', lon: '' }
+    this.state = { lat: '', lon: '', label: '' }
   }
 
   render() {
     const { onClose, onAddUserSite } = this.props
-    const { lat, lon } = this.state
+    const { lat, lon, label } = this.state
 
     return (
       <>
@@ -47,6 +48,19 @@ export default class AddUserSite extends React.Component<AddUserSiteProps, AddUs
             />
           </label>
         </div>
+        <div>
+          <label>
+            <div>Name</div>
+            <input
+                className="input is-small"
+                type="text"
+                data-lpignore="true"
+                value={label}
+                style={{width: '183px', textAlign: 'right', marginBottom: '24px'}}
+                onChange={e => this.setState({label: e.target.value})}
+            />
+          </label>
+        </div>
         <div style={{ textAlign: 'right', paddingRight: '15px' }}>
           <button
             className="button"
@@ -68,7 +82,7 @@ export default class AddUserSite extends React.Component<AddUserSiteProps, AddUs
               const lonF = parseFloat(lon)
 
               if (latF && lonF) {
-                onAddUserSite(latF, lonF)
+                onAddUserSite(latF, lonF, label)
               }
             }}
           >

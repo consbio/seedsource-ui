539200591af74b42522cfbf5e9480467a95f2b50
Jesudas Joseph <jess.joseph@consbio.org>
2022-10-26 08:25:17 -0700

    Changed legend labels to show default labels except 'data/Match' layer

--- src/actions/legends.ts
+++ src/actions/legends.ts
@@ -15,17 +15,22 @@ export const requestLayersLegend = () => {
 }
 
 export const receiveLayersLegend = (json: any) => {
-  const { legend } = json.layers[0]
-  const values = legend.map((element: any) => {
-    return Number.parseFloat(element.label)
-  }).filter((number: any) => !Number.isNaN(number))
+  const values = json.layers[0].legend
+    .map((element: any) => {
+      return Number.parseFloat(element.label)
+    })
+    .filter((number: any) => !Number.isNaN(number))
 
-  return {
+  const action = {
     type: RECEIVE_LAYERS_LEGEND,
-    legend: json.layers[0].legend.map((element: any) => {
+    legend: json.layers[0].legend,
+    layerName: json.layers[0].layerName,
+  }
+
+  if (json.layers[0].layerName === 'data') {
+    action.legend = json.layers[0].legend.map((element: any) => {
       const number = Number.parseFloat(element.label)
       let label = ''
-
       if (number === Math.min(...values)) {
         label = 'Low'
       } else if (number === Math.max(...values)) {
@@ -36,7 +41,7 @@ export const receiveLayersLegend = (json: any) => {
         ...element,
         label,
       }
-    }),
-    layerName: json.layers[0].layerName,
+    })
   }
+  return action
 }
